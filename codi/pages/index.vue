661e1e9bb9832fffe4f95da36a1275c48f0f1f92
JordiCar-design <jordicll@gmail.com>
2023-01-17 15:04:17 +0100

    1er commit

---
+++ codi/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <v-row>
+    <h1>En construcció</h1>
+  </v-row>
+  </v-container>
+  
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>

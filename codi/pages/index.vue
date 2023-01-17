dc56c8c5b0778d1f3afc2a6f546ad80f2775284a
JordiCar-design <jordicll@gmail.com>
2023-01-17 19:00:55 +0100

    Commit final del dia

--- codi/pages/index.vue
+++ codi/pages/index.vue
@@ -1,51 +1,52 @@
 <template>
   <v-container>
     <v-row>
-      <v-col>
-        <h1>EN CONSTRUCCIÓ</h1>
-      </v-col>
-      <!-- <v-col cols="4"> 
-        <v-text-field label="text"></v-text-field>
-        <v-btn ></v-btn>
+      <v-col cols="4"> 
+        <v-text-field v-model="missatge" label="missatge"></v-text-field>
       </v-col>
       <v-col cols="4">
-        <v-text-field label="nomUsuari"></v-text-field>
-        <v-btn ></v-btn>
+        <v-text-field v-model="usuari" label="usuari"></v-text-field>
       </v-col>
       <v-col cols="4">
-        <v-btn >Enviar</v-btn>
+        <v-btn @click="enviarMissatge()">Enviar</v-btn>
       </v-col>
     </v-row>
     <v-row>
-      <v-col>
-        <h1>En construcció</h1>
+      <v-col> 
+        <!-- <h1>En construcció</h1> -->
         <h1 style="font-size:10vh">{{ultimMissatge}}</h1>
         <h4>{{ultimUsuari}}</h4>
-        <v-btn @click="enviarMissatge()">enviar</v-btn>
+        <!-- <v-btn @click="enviarMissatge()">enviar</v-btn> -->
 
          <v-btn @click="descarregarUltimText()">ultim text</v-btn> 
-      </v-col> -->
+      </v-col>
     </v-row>
   </v-container>
   </template>
   
-  <!-- <script> 
+
+  <script> 
   export default{
     mounted(){
 
+      let self = this
+
       setInterval(function() {
-        console.log("hola")
-      }, 5000);
-      this.descarregarUltimText()
+       self.descarregarUltimText()
+      }, 1000);
+      
     },
     data(){
       return{
         respostaTest:null,
         ultimMissatge:"",
-        ultimUsuari:""
+        ultimUsuari:"",
+        missatge:"",
+        usuari:""
       }
     },
     methods:{
+      
       testejarApi(){
         console.log('Provant api..')
         let self = this
@@ -70,15 +71,13 @@
         this.$axios.get('https://last-one-smo-4w3qk.ondigitalocean.app/api/getText')
         .then(
           r=>{
-            console.log("S'ha enviat bé")
             // Recollim
             let missatge = r.data.data.missatge
             let usuari = r.data.data.usuari
             // Assignem al data
             self.ultimMissatge = missatge
             self.ultimUsuari = usuari
-          }
-          
+          }  
         )
         .catch(
           e=>{
@@ -90,8 +89,8 @@
         let self = this
 
         let dades = {
-          missatge:"Hola sóc en Jordi C",
-          usuari:"J.C."
+          missatge:this.missatge,
+          usuari:this.usuari
         }
 
         let url = 'https://last-one-smo-4w3qk.ondigitalocean.app/api/setText'
@@ -111,4 +110,4 @@
       }
     }
   }
-  </script> -->
+  </script>

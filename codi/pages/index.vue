7030bc3393bb7edb5bc035dcfeddcc7de4847023
JordiCar-design <jordicll@gmail.com>
2023-01-17 16:23:11 +0100

    3er commit

--- codi/pages/index.vue
+++ codi/pages/index.vue
@@ -2,8 +2,14 @@
   <v-container>
     <v-row>
       <v-col>
-    <h1 style="font-size:10vh"> AQUÍ VA EL MISSATGE</h1>
-    <v-btn @click="testejarApi()">test</v-btn>
+        <div v-if="respostaTest">
+          <h1 style="font-size:10vh"> AQUÍ VA EL MISSATGE</h1>
+          <pre>
+          {{ respostaTest.data.info }}
+        </pre>
+        
+        </div>
+        <v-btn @click="testejarApi()">test</v-btn>
      </v-col>
     </v-row>
   </v-container>
@@ -12,10 +18,29 @@
 
 <script>
 export default {
+  data(){
+    return{
+      respostaTest:null
+    }
+  },
   methods:{
     testejarApi(){
       console.log('Provant api...')
-      
+      let self = this
+      this.$axios.get('https://last-one-smo-4w3qk.ondigitalocean.app/api')
+      // Quan acabi
+      .then(
+        resposta=>{
+          console.log("M'he descarregat les dades bé", resposta.data)
+          self.respostaTest = resposta
+        }
+      )
+      // Si hi ha errors
+      .catch(
+        error=>{
+          console.log("M'he descarregat les dades malament", error)
+        }      
+      )
     }
   }
 }

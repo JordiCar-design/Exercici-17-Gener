5f573a02214049541e7370113d00fd6fab4f1a48
JordiCar-design <jordicll@gmail.com>
2023-01-17 17:43:03 +0100

    Commit setInterval

--- codi/pages/index.vue
+++ codi/pages/index.vue
@@ -3,8 +3,10 @@
     <v-row>
       <v-col>
         <h1 style="font-size:10vh">{{ultimMissatge}}</h1>
-        <h4>{{ultimUsuari}}</h4>  
-        <v-btn @click="descarregarUltimText()">ultim text</v-btn>
+        <h4>{{ultimUsuari}}</h4>
+        <v-btn @click="enviarMissatge()">enviar</v-btn>
+
+        <!-- <v-btn @click="descarregarUltimText()">ultim text</v-btn> -->
       </v-col>
     </v-row>
   </v-container>
@@ -12,6 +14,13 @@
   
   <script>
   export default{
+    mounted(){
+
+      setInterval(function() {
+        console.log("hola")
+      }, 5000);
+      this.descarregarUltimText()
+    },
     data(){
       return{
         respostaTest:null,
@@ -44,7 +53,7 @@
         this.$axios.get('https://last-one-smo-4w3qk.ondigitalocean.app/api/getText')
         .then(
           r=>{
-            console.log('Ultim text',r)
+            console.log("S'ha enviat bé")
             // Recollim
             let missatge = r.data.data.missatge
             let usuari = r.data.data.usuari
@@ -52,12 +61,36 @@
             self.ultimMissatge = missatge
             self.ultimUsuari = usuari
           }
+          
         )
         .catch(
           e=>{
-            console.log('Ultim text error',e)
+            console.log('Ultim text error', e)
           }
         )
+      },
+      enviarMissatge(){
+        let self = this
+
+        let dades = {
+          missatge:"Hola sóc en Jordi C",
+          usuari:"J.C."
+        }
+
+        let url = 'https://last-one-smo-4w3qk.ondigitalocean.app/api/setText'
+
+        this.$axios.post(url, dades)
+          .then(
+            r=>{
+              console.log("S'ha enviat bé")
+              self.descarregarUltimText()
+            }
+          )
+          .catch(
+            e=>{
+
+            }
+          )
       }
     }
   }
